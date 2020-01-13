ac0393d369369efcd58ef01029c5f27e2efe8fc1
avishekmaitra <amaitra101@gmail.com>
2020-01-13 15:36:13 -0800

    Moving RTC IRQ to RTC file

--- RTC.c
+++ RTC.c
@@ -19,5 +19,28 @@ void initRTC(void)
     // enable RTC time event interrupt every minute
 }
 
+// RTC interrupt service routine
+void RTC_C_IRQHandler(void)
+{
+    //Check if the fields are ready to read
+    if (RTCCTL0 & RTCTEVIFG)
+    {
+        uint16_t currentHour;
+        uint16_t currentMin;
+        currentHour = RTCHOUR & HOUR_MASK;
+        currentMin = RTCMIN & MINUTE_MASK;
+
+        // Convert to decimal value used as an index
+        globalCurrentTime = (currentHour*60) + currentMin;
+
+        // TODO Have function call with current time result to get rid of global variable or keep depending on application
+
+        // Unlock and clear interrupt event
+        RTCCTL0_H = RTCKEY_H ;
+        RTCCTL0_L &= ~RTCTEVIFG;
+        RTCCTL0_H = 0;
+    }
+}
+
 
 
