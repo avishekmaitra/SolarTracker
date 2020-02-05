462cde02aa177719facd1824f467a2c888820a5f
avishekmaitra <amaitra101@gmail.com>
2020-02-05 13:01:19 -0800

    Added multi byte functionality

--- I2C.c
+++ I2C.c
@@ -32,7 +32,7 @@ void InitI2C(uint8_t DeviceAddress)
 }
 
 //Function that writes a single byte to I2C
-void WriteI2C(uint8_t MemAddress, uint8_t MemByte)
+void WriteI2C_SingleByte(uint8_t MemAddress, uint8_t MemByte)
 {
     EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TR;          // Set transmit mode (write)
     EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;       // I2C start condition
@@ -43,7 +43,7 @@ void WriteI2C(uint8_t MemAddress, uint8_t MemByte)
 
     while (!TransmitFlag);            // Wait for the transmit to complete
     TransmitFlag = 0;
-    EUSCI_B0 -> TXBUF = MemByte;      // Send the byte to store in EEPROM
+    EUSCI_B0 -> TXBUF = MemByte;      // Send the byte to store in ACCEL
 
     while (!TransmitFlag);            // Wait for the transmit to complete
     TransmitFlag = 0;
@@ -51,7 +51,7 @@ void WriteI2C(uint8_t MemAddress, uint8_t MemByte)
 }
 
 //Function that reads a single byte from the I2C
-uint8_t ReadI2C(uint8_t MemAddress)
+uint8_t ReadI2C_SingleByte(uint8_t MemAddress)
 {
     uint8_t ReceiveByte;
 
@@ -82,6 +82,76 @@ uint8_t ReadI2C(uint8_t MemAddress)
 
     return ReceiveByte;
 }
+
+//Function that writes a two bytes to I2C
+void WriteI2C_MultiByte(uint8_t MemAddress, uint16_t MemByte)
+{
+    uint8_t HiByte;
+    uint8_t LoByte;
+
+    HiByte = MemByte >> 8;
+    LoByte = MemByte & 0xFF;
+
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TR;          // Set transmit mode (write)
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;       // I2C start condition
+
+    while (!TransmitFlag);            // Wait for I2C address to transmit
+    TransmitFlag = 0;
+    EUSCI_B0 -> TXBUF = MemAddress;   // Send the memory address
+
+    while (!TransmitFlag);            // Wait for the transmit to complete
+    TransmitFlag = 0;
+    EUSCI_B0 -> TXBUF = HiByte;       // Send the byte to store in ACCEL
+
+    while (!TransmitFlag);            // Wait for the transmit to complete
+    TransmitFlag = 0;
+    EUSCI_B0 -> TXBUF = LoByte;       // Send the byte to store in ACCEL
+
+    while (!TransmitFlag);            // Wait for the transmit to complete
+    TransmitFlag = 0;
+    EUSCI_B0 -> CTLW0 |= EUSCI_B_CTLW0_TXSTP;   // I2C stop condition
+}
+
+//Function that reads a single byte from the I2C
+uint16_t ReadI2C_MultiByte(uint8_t MemAddress)
+{
+    uint16_t ReceiveBytes;
+    uint8_t LoByte;
+    uint8_t HiByte;
+
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TR;      // Set transmit mode (write)
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;   // I2C start condition
+
+    while (!TransmitFlag);                    // Wait for I2C address to transmit
+    TransmitFlag = 0;
+
+    EUSCI_B0 -> TXBUF = MemAddress;           // Send the byte of the memory address
+
+    while (!TransmitFlag);                    // Wait for the transmit to complete
+    TransmitFlag = 0;
+
+    EUSCI_B0->CTLW0 &= ~EUSCI_B_CTLW0_TR;     // Set receive mode (read)
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;   // I2C start condition (restart)
+
+    // Wait for start to be transmitted
+    while ((EUSCI_B0->CTLW0 & EUSCI_B_CTLW0_TXSTT));
+
+    while (!TransmitFlag);            // Wait to receive a byte
+    TransmitFlag = 0;
+    HiByte = EUSCI_B0->RXBUF;
+
+    // set stop bit to trigger after second byte
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
+
+    while (!TransmitFlag);            // Wait to receive a byte
+    TransmitFlag = 0;
+    LoByte = EUSCI_B0->RXBUF;
+
+    ReceiveBytes = ((uint16_t)HiByte << 8) | LoByte;
+
+    return ReceiveBytes;
+}
+
 
 //I2C ISR
 void EUSCIB0_IRQHandler(void)
--- I2C.h
+++ I2C.h
@@ -3,7 +3,9 @@
 #include <stdint.h>
 
 void InitI2C(uint8_t DeviceAddress);
-void WriteI2C(uint8_t MemAddress, uint8_t MemByte);
-uint8_t ReadI2C(uint8_t MemAddress);
+void WriteI2C_SingleByte(uint8_t MemAddress, uint8_t MemByte);
+uint8_t ReadI2C_SingleByte(uint8_t MemAddress);
+void WriteI2C_MultiByte(uint8_t MemAddress, uint16_t MemByte);
+uint16_t ReadI2C_MultiByte(uint8_t MemAddress);
 
 #endif /* I2C_H_ */
