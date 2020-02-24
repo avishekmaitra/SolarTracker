d41f6e1c1e4f151e9496dda7227480e1f91233e6
Helen Rice <helen@Helens-MacBook-Air-5.local>
2020-02-23 16:19:34 -0800

    LCD

--- LCD.c
+++ LCD.c
@@ -5,4 +5,98 @@
  *      Author: helen
  */
 
+#include "msp.h"
+#include "delay.h"
+#include "LCD.h"
 
+void init_LCD(void){
+    // setup GPIO pins
+    //P4-> SEL0 &= ~(DB4 | DB5 | DB6 | DB7);  // init p4 pins
+    //P4-> SEL1 &= ~(DB4 | DB5 | DB6 | DB7);  // GPIO setup
+    P4-> DIR |= (DB4 | DB5 | DB6 | DB7);    // set pins to output mode
+    P4-> OUT &= ~(DB4 | DB5 | DB6 | DB7);   // clear pins
+
+    //P3-> SEL0 &= ~(RS | RW | EN);   // init p3 pins
+    //P3-> SEL1 &= ~(RS | RW | EN);   // GPIO setup
+    P3-> DIR |= (RS | RW | EN);     // set pins to output mode
+    P3-> OUT &= ~(RS | RW | EN);    // clear pins
+
+    delay_us(40000, FREQ_12_MHZ);   // power up delay
+
+    P4-> OUT |= 0x30;
+
+    delay_us(5000, FREQ_12_MHZ);    // 5ms delay
+    nybble();
+    delay_us(160, FREQ_12_MHZ);     // 160us delay
+    nybble();
+    delay_us(160, FREQ_12_MHZ);     // 160us delay
+    nybble();
+    delay_us(160, FREQ_12_MHZ);     // 160us delay
+
+    P4-> OUT |= 0x20;
+    nybble();
+
+    //function set
+    command_LCD(FUNC_SET);
+    command_LCD(FUNC_SET);      // 4bit, 2 line, 5x8 font
+    command_LCD(0x10);          // set cursor
+    command_LCD(DISPLAY_ON);    // set display, cursor, blinking
+    command_LCD(CURSOR_RIGHT);  // set cursor entry mode
+}
+
+void clear_LCD(void) {
+    command_LCD(CLR_DISP);
+    delay_us(2000, FREQ_12_MHZ);            // delay 2ms
+}
+
+void command_LCD(unsigned char command) {
+    //P4-> OUT |= command;
+    P4-> OUT &= ~(DB4 | DB5 |
+                  DB6 | DB7);
+    P4-> OUT |= command & (DB4 | DB5 |
+                           DB6 | DB7);  // put data on OUT port
+    P3-> OUT &= ~RS;                    // D/I low
+    P3-> OUT &= ~RW;                    // R/W = 0 --> Write
+
+    nybble();
+
+    //command = command << 4;
+    P4-> OUT |= (command << 4);
+
+    nybble();
+}
+
+void write_char_LCD(unsigned char letter) {
+
+    P4-> OUT &= ~(DB4 | DB5 |
+                  DB6 | DB7);           // put data on OUT port
+    P4-> OUT |= letter & (DB4 | DB5 |
+                          DB6 | DB7);   // put data on output port
+    P3-> OUT |= RS;                     // D/I = high, send data
+    P3-> OUT &= ~RW;                    // R/W = 0 --> Write
+
+    nybble();
+
+    //letter = letter << 4;
+    P4-> OUT &= ~(DB4|DB5|DB6|DB7);
+    P4-> OUT |= (letter << 4) & (DB4|DB5|DB6|DB7);
+
+    nybble();
+
+    //P3-> OUT &= ~RS;                        // register select low
+}
+
+/*
+ * void write_string_LCD(char* string) {       // loop until printed each char of string
+    while(*string) {
+        write_char_LCD(*string);            // write to LCD
+        string++;                           // count "up" next char in string
+    }
+}
+*/
+
+void nybble(void) {
+    P3-> OUT |= EN;                         // enable high
+    delay_us(100, FREQ_12_MHZ);             // delay 0.1ms
+    P3-> OUT &= ~EN;                        // enable low
+}
--- LCD.h
+++ LCD.h
@@ -2,12 +2,42 @@
  * LCD.h
  *
  *  Created on: Feb 2, 2020
- *      Author: helen
+ *      Author:
  */
 
 #ifndef LCD_H_
+#include <stdint.h>
 #define LCD_H_
 
+//  1   2  3   4   5     6    7   8   9  10   11   12  13    14   15   16
+// VSS VDD V0  RS  RW   EN   DB0 DB1 DB2 DB3  DB4  DB5  DB6  DB7 LED+ LED-
+// GND 3V3    P3.5 P3.6 P3.7                 P4.0 P4.1 P4.2 P4.3  5V  GND
+
+#define RS BIT5         // P3.5 Register Select.
+#define RW BIT6         // P3.6 Read/Write Select. RW=1: Read, RW=0: Write
+#define EN BIT7         // P3.7 Operation Enable. falling edge triggered
+
+#define DB4 BIT0        // P4.0 data bit 0
+#define DB5 BIT1        // P4.1
+#define DB6 BIT2        // P4.2
+#define DB7 BIT3        // P4.3
+
+#define CLR_DISP        0x01    // clear display
+#define RETURN_HOME     0x02    // cursor to top left position
+#define CURSOR_LEFT     0x04    // move cursor left, DDRAM decreased
+#define CURSOR_RIGHT    0x06    // move cursor right, DDRAM increased
+#define DISPLAY_ON      0x0F    // display on, cursor on, blinking cursor on
+#define FUNC_SET        0x28    // 4 bit, 1 line, 5x8 font
+#define WRITE_DATA      0x200   // write data on | D7-D0 address
+#define READ_DATA       0x300   // read data from ram
+
+void init_LCD(void);                        // initialize LCD
+void clear_LCD(void);                       // clear display
+void command_LCD(unsigned char command);    // set LCD properties
+void write_char_LCD(unsigned char letter);  // write character to LCD
+void write_string_LCD(char* string);        // write string to LCD
+void home_LCD(void);                        // move cursor top left
+void nybble(void);
 
 
 #endif /* LCD_H_ */
