53ca738d8d5ff08d08d3e9b222b2ec006973dd49
unknown <delaneyberger@yahoo.com>
2020-02-23 18:30:45 -0800

    skeleton

---
+++ UserInterface.c
@@ -0,0 +1,102 @@
+#include "Keypad.h"
+#include "delay.h"
+#include "msp.h"
+#include <stdint.h>
+#include "UART.h"
+
+
+
+void Start_screen(void)
+{
+
+}
+
+void Home_screen(void)
+{
+    /*
+    LCD_write('HOME SCREEEN');
+    LCD_newline;
+    LCD_write('A: Manual Input');
+    LCD_newline;
+    LCD_write('B: Algorithm Based');
+    LCD_newline;
+    LCD_write('C: DEMO');
+    */
+}
+
+void Select_modes(void)
+{
+   switch(Keypad_GetKey())
+    {
+        case 65:                                //Keypress = A (manual mode)
+        {
+            Manual_input();
+        }
+        break;
+
+        case 66:                                //Keypress = B (algorithm mode)
+        {
+            Algorithm_based();
+        }
+        break;
+
+        case 67:                                //Keypress = C (DEMO mode)
+        {
+            Demo();
+        }
+        break;
+
+        case 42:                                //Keypress = * (Back button also clear)
+        {
+            back();
+        }
+        break;
+
+        case 35:                                //Keypress = # (enter)
+        {
+            enter();
+        }
+        break;
+
+        case 46:                               //Keypress = . (Home button)
+        {
+            Home_screen();
+        }
+        break;
+
+        default:
+        {
+            break;
+        }
+}
+
+void Manual_input(void)
+{
+    /*
+    LCD_write('Enter Angle:  ')
+    LCD_newline();
+    LCD_write('*back         .home')
+    */
+}
+
+void Algorithm_based(void)
+{
+    //show angle based on calculation for given time and day
+    //say 'done' and '.Home'
+}
+
+void Demo(void)
+{
+    //display angle from sunrise to sunset 90:-90
+    //done after reaches -90
+}
+
+void back(void)
+{
+
+}
+
+void enter(void)
+{
+
+}
---
+++ UserInterface.h
@@ -0,0 +1,20 @@
+/*
+ * UserInterface.h
+ *
+ *  Created on: Feb 23, 2020
+ *      Author: Delaney Berger
+ */
+
+#ifndef USERINTERFACE_H_
+#define USERINTERFACE_H_
+
+void Start_screen(void);
+void Home_screen(void);
+void Select_modes(void);
+void Manual_input(void);
+void Algorithm_based(void);
+void Demo(void);
+void back(void);
+void enter(void);
+
+#endif /* USERINTERFACE_H_ */
