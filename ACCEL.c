b151332a8d01c51432a1bd5738c36c938fa60bf4
avishekmaitra <amaitra101@gmail.com>
2020-02-24 16:46:25 -0800

    added files

---
+++ ACCEL.c
@@ -0,0 +1,8 @@
+/*
+ * ACCEL.c
+ *
+ *  Created on: Feb 24, 2020
+ *      Author: AviMaitra
+ */
+
+
---
+++ ACCEL.h
@@ -0,0 +1,13 @@
+/*
+ * ACCEL.h
+ *
+ *  Created on: Feb 24, 2020
+ *      Author: AviMaitra
+ */
+
+#ifndef ACCEL_H_
+#define ACCEL_H_
+
+
+
+#endif /* ACCEL_H_ */
