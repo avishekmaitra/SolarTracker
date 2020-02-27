aba71cf056d1a771d69cd0369ae126a05eafc010
Helen Rice <helen@Helens-MacBook-Air-5.local>
2020-02-26 21:18:20 -0800

    Partway Pretty

--- LCD.c
+++ LCD.c
@@ -9,63 +9,60 @@
 #include "delay.h"
 #include "LCD.h"
 
-void init_LCD(void){
-    // setup GPIO pins
-    P4-> SEL0 &= ~(DB4 | DB5 | DB6 | DB7);  // init p4 pins
-    P4-> SEL1 &= ~(DB4 | DB5 | DB6 | DB7);  // GPIO setup
-    P4-> DIR |= (DB4 | DB5 | DB6 | DB7);    // set pins to output mode
+void init_LCD(void)                         //setup GPIO pins
+    {
+    P4-> SEL0 &= ~(DB4 | DB5 | DB6 | DB7);  //initialize P4 pins
+    P4-> SEL1 &= ~(DB4 | DB5 | DB6 | DB7);  //GPIO setup
+    P4-> DIR |=   (DB4 | DB5 | DB6 | DB7);  //set pins to output mode
 
-    P3-> SEL0 &= ~(RS | RW | EN);           // init p3 pins
-    P3-> SEL1 &= ~(RS | RW | EN);           // GPIO setup
-    P3-> DIR |= (RS | RW | EN);             // set pins to output mode
+    P3-> SEL0 &= ~(RS | RW | EN);           //initialize P3 pins
+    P3-> SEL1 &= ~(RS | RW | EN);           //GPIO setup
+    P3-> DIR |= (RS | RW | EN);             //set pins to output mode
 
     delay_ms(2,FREQ_48_MHZ);
 
-    P4-> OUT = 0x00;
+    P4-> OUT = 0x00;                        //clear outputs
     P3-> OUT = 0x00;
-    delay_ms(100,FREQ_48_MHZ);              // power up delay
+    delay_ms(100,FREQ_48_MHZ);              //power up delay
     P4 -> OUT = 0x30;
 
-    delay_ms(30,FREQ_48_MHZ);               // power up delay
+    delay_ms(30,FREQ_48_MHZ);               //power up delay
     nybble();
     delay_ms(10,FREQ_48_MHZ);
     nybble();
     delay_ms(10,FREQ_48_MHZ);
     nybble();
     delay_ms(10,FREQ_48_MHZ);
+
     P4 -> OUT = 0x20;
     nybble();
 
-    command_LCD(FUNC_SET);                  // 4bit, 2 line, 5x8 font
-    command_LCD(SET_CURSOR);                // set cursor
-    command_LCD(DISPLAY_ON);                // set display, cursor, blinking
-    command_LCD(CURSOR_RIGHT);              // set cursor direction
+    command_LCD(FUNC_SET);                  //4 bit, 2 line, 5x8 font
+    command_LCD(SET_CURSOR);                //set cursor
+    command_LCD(DISPLAY_ON);                //set display, cursor, blinking
+    command_LCD(CURSOR_RIGHT);              //set cursor direction
 
     delay_ms(2,FREQ_48_MHZ);
 }
 
-void clear_LCD(void)
+void clear_LCD(void)                        //clears all characters from screen
 {
     delay_ms(2, FREQ_48_MHZ);
     command_LCD(CLR_DISP);
-    delay_ms(2, FREQ_48_MHZ);               // delay 2ms
+    delay_ms(2, FREQ_48_MHZ);
 }
 
-void command_LCD(unsigned char command)
+void command_LCD(unsigned char command)     //reads instruction codes
 {
-
     delay_ms(2,FREQ_48_MHZ);
 
-    P4-> OUT = command;                     // put data on OUT port
-
-    P3-> OUT &= ~RS;                        // D/I low
-    P3-> OUT &= ~RW;                        // R/W = 0 --> Write
-
+    P4-> OUT = command;                     //put instruction on OUT
+    P3-> OUT &= ~RS;                        //D/I = low --> Send Instruction
+    P3-> OUT &= ~RW;                        //R/W = low --> Write
     nybble();
 
-    command = command << 4;                 // analyze upper 4 bits
-    P4-> OUT = command;
-
+    command = command << 4;                 //analyze next 4 bits
+    P4-> OUT = command;                     //shifted instruction code on OUT
     nybble();
 
     delay_ms(2,FREQ_48_MHZ);
@@ -74,67 +71,70 @@ void command_LCD(unsigned char command)
 void write_char_LCD(unsigned char letter)
 {
     delay_ms(2,FREQ_48_MHZ);
-    P4 -> OUT = letter;                     // put data on output port
-    P3-> OUT |= RS;                         // D/I = high, send data
-    P3-> OUT &= ~RW;                        // R/W = 0 --> Write
 
+    P4 -> OUT = letter;                     //put letter data on OUT
+    P3-> OUT |= RS;                         //D/I = high --> Send Data
+    P3-> OUT &= ~RW;                        //R/W = low --> Write
     nybble();
 
-    letter = letter << 4;                   // analyze upper 4 bits
-    P4 -> OUT = letter;
-
+    letter = letter << 4;                   //analyze next 4 bits
+    P4 -> OUT = letter;                     //shifted letter data on OUT
     nybble();
 
     delay_ms(2,FREQ_48_MHZ);
 }
 
 void write_string_LCD(char* string)
-{       // loop until printed each char of string
-    while(*string)
+{
+    while(*string)                          //loop until string completed
     {
-        write_char_LCD(*string);            // write to LCD
-        string++;                           // count "up" next char in string
+        write_char_LCD(*string);            //write to LCD
+        string++;                           //count "up" to next char in string
     }
 }
 
-void SetCursorLocation(char place)
-{
-    //Set cursor to proper location as defined by the STU data sheet
+void SetCursorLocation(char place)          //set cursor to DDRAM location as defined
+{                                           //by the STU data sheet for 2-line mode
     char myCommand = ENABLE_MOVE + place;
     command_LCD(myCommand);
 }
 
-void write_LCD_L1(char* string) {
-    SetCursorLocation(0x00);                // set cursor location to DDRAM address 0x00, line 1
-    write_string_LCD(string);               // string to be assigned to line 1
+void write_LCD_L1(char* string)
+{
+    SetCursorLocation(0x00);                //set cursor location to DDRAM address 0x00, line 1
+    write_string_LCD(string);               //string to be assigned to line 1
 }
 
-void write_LCD_L2(char* string) {
-    SetCursorLocation(0x40);                // set cursor location to DDRAM address 0x40, line 2
-    write_string_LCD(string);               // string to be assigned to line 2
+void write_LCD_L2(char* string)
+{
+    SetCursorLocation(0x40);                //set cursor location to DDRAM address 0x40, line 2
+    write_string_LCD(string);               //string to be assigned to line 2
 }
 
-void write_LCD_L3(char* string) {
-    SetCursorLocation(0x14);                // set cursor location to DDRAM address 0x14, line 3
-    write_string_LCD(string);               // string to be assigned to line 3
+void write_LCD_L3(char* string)
+{
+    SetCursorLocation(0x14);                //set cursor location to DDRAM address 0x14, line 3
+    write_string_LCD(string);               //string to be assigned to line 3
 }
 
-void write_LCD_L4(char* string) {
-    SetCursorLocation(0x54);                // set cursor location to DDRAM address 0x54, line 4
-    write_string_LCD(string);               // string to be assigned to line 4
+void write_LCD_L4(char* string)
+{
+    SetCursorLocation(0x54);                //set cursor location to DDRAM address 0x54, line 4
+    write_string_LCD(string);               //string to be assigned to line 4
 }
 
-void nybble(void) {
-    P3-> OUT |= EN;                         // enable high
-    delay_ms(1, FREQ_48_MHZ);               // delay 0.1ms
-    P3-> OUT &= ~EN;                        // enable low
+void nybble(void)
+{
+    P3-> OUT |= EN;                         //enable high
+    delay_ms(1, FREQ_48_MHZ);               //delay 0.1ms
+    P3-> OUT &= ~EN;                        //enable low
 }
 
-void StartScreen(void) {
-    // STARTING SCREEN
-    write_LCD_L1("Startup             ");            // move everything up, get rid of this
-    write_LCD_L2("Enter Date:__/__/__ ");            // remember: only have 20 char spaces
-    write_LCD_L3("Enter Time:__:__    ");            // military time
+void StartScreen(void)                      //STARTING SCREEN
+{
+    write_LCD_L1("Startup             ");
+    write_LCD_L2("Enter Date:__/__/__ ");   //remember: only have 20 char spaces
+    write_LCD_L3("Enter Time:__:__    ");   //military time
     write_LCD_L4("*Clear        #Enter");
     SetCursorLocation(0x4B);
     //getkeypress function
@@ -143,65 +143,66 @@ void StartScreen(void) {
     //needs to wait for either Clear or Enter press before changing screens
 }
 
-void HomeScreen(void) {
-    // HOME SCREEN
-    write_LCD_L1("A: Manual Entry     ");
+void HomeScreen(void)                       //HOME MODE CHOICE SCREEN
+{
+    write_LCD_L1("A: Manual Entry     ");   //A, B, or C key press will decide next LCD screen
     write_LCD_L2("B: Algorithm Based  ");
     write_LCD_L3("C: Demo Mode        ");
     write_LCD_L4("*Back               ");
 }
 
-void A_1_MANUAL(void) {
-    // A_1: MANUAL ENTRY INPUT
+void A1_MANUAL(void)                        //A_1: MANUAL ENTRY INPUT SCREEN
+{
     write_LCD_L1("                    ");
     write_LCD_L2("Enter Angle: ___deg ");
     write_LCD_L3("                    ");
-    write_LCD_L4("*Back  !Home  #Enter");           // Back deletes input value, Home goes to Home, Enter verifies angle desired
+    write_LCD_L4("*Back  !Home  #Enter");   //Back deletes input value, Home goes Home, Enter verifies angle desired
 }
 
-void A_2_MANUAL(void) {
-    // A_2: ANGLE UPDATE
+void A2_MANUAL(void)                        //A_2: MANUAL ENTRY ANGLE UPDATE SCREEN
+{
     write_LCD_L1("Present Angle:___deg");
     write_LCD_L2("                    ");
-    write_LCD_L3("Done.               ");           // only displayed when reach angle
+    write_LCD_L3("Done.               ");   //only displayed when reach desired angle
     write_LCD_L4("*Back  !Home        ");
 }
 
-void B_1_ALGORITHM(void) {
-    // B_1: ALGORITHM BASED
+void B1_ALGORITHM(void)                     //B_1: ALGORITHM BASED ENTRY SCREEN
+{
     write_LCD_L1("                    ");
     write_LCD_L2("                    ");
     write_LCD_L3("                    ");
     write_LCD_L4("*Back  !Home  #Enter");
 }
 
-void B_2_ALGORITHM(void) {
-    // B_2: ANGLE UPDATE
+void B2_ALGORITHM(void)                     //B_2: ALGORITHM ANGLE UPDATE SCREEN
+{
     write_LCD_L1("Present Time:__:__  ");
     write_LCD_L2("Present Angle:___deg");
-    write_LCD_L3("Done.               ");
+    write_LCD_L3("Done.               ");   //only displayed when reach desired angle
     write_LCD_L4("*Back  !Home        ");
 }
 
-void C_1_DEMO(void) {
-    // C_1: DEMO
+void C1_DEMO(void)                          //C_1: DEMO choice screen
+{
     write_LCD_L1("Choose Demo Speed:  ");
     write_LCD_L2("A, B, C             ");
     write_LCD_L3("                    ");
     write_LCD_L4("       !Home  #Enter");
 }
 
-void C_2_DEMO(void) {
+void C2_DEMO(void)                          //C_2: DEMO UPDATE SCREEN
+{
     write_LCD_L1("Percent Complete:__%");
     write_LCD_L2("Present Angle:___deg");
-    write_LCD_L3("Done.               ");
+    write_LCD_L3("Done.               ");   //only displayed when reach desired angle
     write_LCD_L4("*Back          !Home");
 }
 
-void lcd_test(void) {
-    // write LCD Test
-    write_LCD_L1("This now kinda worksz");           // resolved
-    write_LCD_L2("This now kindb works");           // resolved
+void lcd_test(void)                         //write LCD Test
+{
+    write_LCD_L1("This now kinda works");
+    write_LCD_L2("This now kindb works");
     write_LCD_L3("This now kindc works");
     write_LCD_L4("This now kindd works");
 }
--- LCD.h
+++ LCD.h
@@ -9,32 +9,34 @@
 #include <stdint.h>
 #define LCD_H_
 
-//  1   2  3    4   5     6    7   8   9  10   11   12   13    14   15   16
-// VSS VDD V0   RS  RW   EN   DB0 DB1 DB2 DB3  DB4  DB5  DB6   DB7  LED+ LED-
-// GND 3V3 GND P3.5 P3.6 P3.7                 P4.4  P4.5 P4.6  P4.7  5V  GND
+// LCD Pinout
+// 1   2   3   4    5    6    7    8    9    10   11   12   13   14   15   16
+// VSS VDD V0  RS   RW   EN   DB0  DB1  DB2  DB3  DB4  DB5  DB6  DB7  LED+ LED-
+// GND 3V3 GND P3.5 P3.6 P3.7                     P4.4 P4.5 P4.6 P4.7 5V   GND
 
-#define RS BIT5         // P3.5 Register Select.
-#define RW BIT6         // P3.6 Read/Write Select. RW=1: Read, RW=0: Write
-#define EN BIT7         // P3.7 Operation Enable. falling edge triggered
+#define RS BIT5                             // P3.5 Register Select. RS=1: Send Data, RS=0: Send Instr
+#define RW BIT6                             // P3.6 Read/Write Select. RW=1: Read, RW=0: Write
+#define EN BIT7                             // P3.7 Operation Enable. falling edge triggered
 
-#define DB4 BIT4        // P4.4 data bit 0
-#define DB5 BIT5        // P4.5
-#define DB6 BIT6        // P4.6
-#define DB7 BIT7        // P4.7
+#define DB4 BIT4                            // P4.4
+#define DB5 BIT5                            // P4.5
+#define DB6 BIT6                            // P4.6
+#define DB7 BIT7                            // P4.7
 
-#define CLR_DISP        0x01    // clear display
-#define RETURN_HOME     0x02    // cursor to top left position
-#define CURSOR_LEFT     0x04    // move cursor left, DDRAM decreased
-#define CURSOR_RIGHT    0x06    // move cursor right, DDRAM increased
-#define DISPLAY_ON      0x0F    // display on, cursor on, blinking cursor on
-#define FUNC_SET        0x28    // 4 bit, 1 line, 5x8 font
-#define WRITE_DATA      0x20    // write data on | D7-D0 address
-#define READ_DATA       0x30    // read data from ram
-#define SET_CURSOR      0x10    // set cursor
+#define CLR_DISP        0x01                // clear display
+#define RETURN_HOME     0x02                // cursor to top left position
+#define CURSOR_LEFT     0x04                // move cursor left, DDRAM decreased
+#define CURSOR_RIGHT    0x06                // move cursor right, DDRAM increased
+#define DISPLAY_ON      0x0F                // display on, cursor on, blinking cursor on
+#define FUNC_SET        0x28                // 4 bit, 2 line, 5x8 font
+#define WRITE_DATA      0x20                // write data on | D7-D0 address
+#define READ_DATA       0x30                // read data from ram
+#define SET_CURSOR      0x10                // set cursor
 #define ENABLE_MOVE     0X80
 
 void init_LCD(void);                        // initialize LCD
 void clear_LCD(void);                       // clear display
+void nybble(void);
 void command_LCD(unsigned char command);    // set LCD properties
 void write_char_LCD(unsigned char letter);  // write character to LCD
 void write_string_LCD(char* string);        // write string to LCD
@@ -44,14 +46,15 @@ void write_LCD_L1(char* string);            // write string to line 1
 void write_LCD_L2(char* string);            // write string to line 2
 void write_LCD_L3(char* string);            // write string to line 3
 void write_LCD_L4(char* string);            // write string to line 4
-void nybble(void);
-void HomeScreen(void);
-void A_1_MANUAL(void);
-void A_2_MANUAL(void);
-void B_1_ALGORITHM(void);
-void B_2_ALGORITHM(void);
-void C_1_DEMO(void);
-void C_2_DEMO(void);
 void lcd_test(void);
+
+void StartScreen(void);
+void HomeScreen(void);
+void A1_MANUAL(void);
+void A2_MANUAL(void);
+void B1_ALGORITHM(void);
+void B2_ALGORITHM(void);
+void C1_DEMO(void);
+void C2_DEMO(void);
 
 #endif /* LCD_H_ */
