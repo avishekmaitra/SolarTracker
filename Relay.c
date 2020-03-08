ef5856eb367c129b437d928eb0e9c89a5a9e4bff
avishekmaitra <amaitra101@gmail.com>
2020-03-08 13:47:05 -0700

    Relay functionality

--- Relay.c
+++ Relay.c
@@ -5,4 +5,85 @@
  *      Author: AviMaitra
  */
 
+#include "ACCEL.h"
+#include "UserInterface.h"
+#include "delay.h"
+#include "msp.h"
+#include <stdbool.h>
 
+#define THRESHOLD 3
+
+enum state
+{
+    OFF,
+    IN,
+    OUT
+};
+
+static uint8_t relayState = OFF;
+
+void Relay_Init(void)
+{
+    //Set 6.6 and 6.7 as outputs
+    P6 -> DIR |= (BIT6|BIT7);
+    P6 -> SEL1 &= ~(BIT6|BIT7);
+    P6 -> SEL0 &= ~(BIT6|BIT7);
+    P6 -> OUT &= ~(BIT6|BIT7);
+}
+
+void Relay_Off(void)
+{
+    P6 -> OUT &= ~(BIT6|BIT7);
+    relayState = OFF;
+}
+
+void Relay_In(void)
+{
+    P6 -> OUT &= ~(BIT6);
+    P6 -> OUT |= (BIT7);
+    relayState = IN;
+}
+
+void Relay_Out(void)
+{
+    P6 -> OUT |= (BIT6);
+    P6 -> OUT &= ~(BIT7);
+    relayState = OUT;
+}
+
+uint8_t Relay_GetState(void)
+{
+    return relayState;
+}
+
+// Returns true if still moving, false when reaches goal
+bool Relay_MoveToGoal(void)
+{
+    int8_t goalAngle,actualAngle;
+    int8_t minThreshold, maxThreshold;
+    bool notAtGoal = true;
+
+    goalAngle = UI_GetGoalAngle();
+    actualAngle = ACCEL_GetAngle_Int();
+
+    minThreshold = goalAngle - THRESHOLD;
+    maxThreshold = goalAngle + THRESHOLD;
+
+    if(actualAngle < minThreshold)
+    {
+        Relay_In();
+        notAtGoal = true;
+    }
+    else if(actualAngle > maxThreshold)
+    {
+        Relay_Out();
+        notAtGoal = true;
+    }
+    // Reached goal
+    else
+    {
+        Relay_Off();
+        notAtGoal = false;
+    }
+    return notAtGoal;
+}
--- Relay.h
+++ Relay.h
@@ -8,6 +8,14 @@
 #ifndef RELAY_H_
 #define RELAY_H_
 
+#include <stdbool.h>
+
+void Relay_Init(void);
+void Relay_Off(void);
+void Relay_In(void);
+void Relay_Out(void);
+uint8_t Relay_GetState(void);
+bool Relay_MoveToGoal(void);
 
 
 #endif /* RELAY_H_ */
