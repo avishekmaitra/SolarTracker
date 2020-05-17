3828dc49d3422b099cad28bc9c0242881c716fb9
avishekmaitra <amaitra101@gmail.com>
2020-05-17 16:29:57 -0700

    more bugfix

--- I2C.h
+++ I2C.h
@@ -10,5 +10,6 @@ void I2C_WriteMultiByte(uint8_t MemAddress, uint16_t MemByte);
 uint16_t I2C_ReadMultiByte(uint8_t MemAddress);
 uint8_t I2C_GetComErrorFlag();
 void I2C_ResetComErrorFlag(void);
+void I2C_SetComErrorFlag(void)
 
 #endif /* I2C_H_ */
